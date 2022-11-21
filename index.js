a1a14580902b2bd19da66e2d4a0a6b602e451933
Alex Zhang <alexzhang@Alexs-MacBook-Pro.local>
2022-11-21 16:20:47 +1100

    first commmit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs=require('fs')
+const xml2js = require('xml2js')
+const util=require('util')
+const express = require ('express')
+const app=express()
+const PORT=5000
+const parser= new xml2js.Parser();
+const cors= require('cors')
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    fs.readFile('LiveHirePublicXmlFeed.xml',(err,data)=>{
+        parser.parseString(data,(err,result)=>{
+            res.json(result.source.job)
+        })
+    })
+})
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log(`app listening on port ${PORT}`)
+})
